2346a95d70640d69b57d84ceaba9158530270fbf
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-02-13 18:57:56 -0800

    Declare variables in js
    All available characters for password and user interaction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Available characters for password
+var numbers = "1234567890".split("");
+var lowcase = "abcdefghijklmnopqrstuvwxyz".split("");
+var uppcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var speChar = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
+var logicArr = [];
+var finalArr = [];
+
+// User interaction 
+
+var passNums = confirm("Would you like your password contain numbers?");
+var passLower = confirm("Should your password contain lowercase letters?");
+var passUpper = confirm("Will your password contain UPPERCASE letters?");
+var passSpec = confirm("And how about some special characters??");
+var passwordLength = prompt("How many characters would you like your password to contain?");
+ 
+// create conditional to pass response to logicArr
+
+var num = [Math.floor(Math.random()*numbers.length)];
+
+
+
+console.log(num)
+console.log(passNums);
+// console.log(passLower);
+// console.log(passUpper);
+// console.log(passSpec);
+// console.log(passwordLength);
+// console.log(num);
+
+alert.passwordLength;
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +45,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
