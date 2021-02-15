0bab1f9d402db77514e5d021bc8ed2e29c0638ab
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-02-14 20:39:24 -0800

    Refactor JS
    Complete and refactor js for cleaner code and commenting

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # passwordGenerator
+
+##  Generate a Password!
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,84 +8,62 @@ var lowCaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p
 var uppCaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var specialArr = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}"];
 
+// temporary array to store choices
+
 var logicArr = [];
+
+// final array to store password
+
 var finalArr = [];
 
 
+// Create a function that when invoked by button press) executes our code prompting the user for their choices
 
 function generatePassword() {
-
-}
-
-
-// ask user desired length of password
-
-var passwordLength = prompt("How many characters would you like your password to contain?");
-
-// ensure user selects a number between 8 and 24 then store that value in password length
-
-if (passwordLength < 8 || passwordLength > 24) {
-  passwordLength = prompt("You must select a number between 8 and 24 characters");
-
-}
-
-// ask user if they would like the password to contain numbers, if true push numbers to logicArr array
-
-var numberChoice = confirm("Would you like your password contain numbers?");
-
-if (numberChoice) {
-  for (var i = 0; i < numbersArr.length; i++) {
-    logicArr.push(numbersArr[i])
+  // ask user desired length of password
+  // ensure user selects a number between 8 and 24 then store that value in password length
+  var passwordLength = prompt("How many characters would you like your password to contain?");
+  if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("You must select a number between 8 and 128 characters");
   }
-}
-
-// ask user if they would like password to contain numbers, if true push to array
-
-var lowerChoice = confirm("Should your password contain lowercase letters?");
-
-if (lowerChoice) {
-  for (var i = 0; i < lowCaseArr.length; i++) {
-    logicArr.push(lowCaseArr[i])
+  // ask user if they would like the password to contain numbers, if true push numbers to logicArr array
+  var numberChoice = confirm("Would you like your password contain numbers?");
+  if (numberChoice) {
+    for (var i = 0; i < numbersArr.length; i++) {
+      logicArr.push(numbersArr[i])
+    }
   }
-}
-
-// ask user to confirm UPPER CASE letters, if true push to array
-
-var upperChoice = confirm("Will your password contain UPPERCASE letters?");
-
-if (upperChoice) {
-  for (var i = 0; i < uppCaseArr.length; i++) {
-    logicArr.push(uppCaseArr[i])
+  // ask user if they would like password to contain numbers, if true push to array
+  var lowerChoice = confirm("Should your password contain lowercase letters?");
+  if (lowerChoice) {
+    for (var i = 0; i < lowCaseArr.length; i++) {
+      logicArr.push(lowCaseArr[i])
+    }
   }
-}
-
-// ask user to confirm special characters, if true push to array
-
-var specialChoice = confirm ("And how about some special characters??");
-
-if (specialChoice) {
-  for (var i = 0; i < specialArr.length; i++) {
-    logicArr.push(specialArr[i])
+  // ask user to confirm UPPER CASE letters, if true push to array
+  var upperChoice = confirm("Will your password contain UPPERCASE letters?");
+  if (upperChoice) {
+    for (var i = 0; i < uppCaseArr.length; i++) {
+      logicArr.push(uppCaseArr[i])
+    }
   }
+  // ask user to confirm special characters, if true push to array
+  var specialChoice = confirm ("And how about some special characters??"); 
+  if (specialChoice) {
+    for (var i = 0; i < specialArr.length; i++) {
+      logicArr.push(specialArr[i])
+    }
+  }
+
+  /* write a for loop that will iterate through logicArr and then use Math.random to push character 
+  strings to final array based on limit set by user in passwordLength using .length*/
+  for (var i = 0; i < passwordLength; i++) {
+    finalArr.push(logicArr[Math.floor(Math.random() * logicArr.length)]);
+  }
+  
+  // join into one array
+  return finalArr.join(" ");
 }
-
-/*we need to write a for loop that will iterate through logicArr 
-and then use Math.random to push character strings to final array based 
-on limit set by user in passwordLength using .length*/
-
-for (var i = 0; i < passwordLength; i++) {
-  finalArr.push(logicArr[Math.floor(Math.random() * logicArr.length)]);
-}
-
-
-
-console.log(logicArr);
-console.log(passwordLength);
-console.log(finalArr);
-
-
-alert.passwordLength;
-
 
 // Write password to the #password input
 function writePassword() {
@@ -98,4 +76,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
